e640125a8720acd445280de624357c03f125b4fc
dawid <kontakt@dgloans.net>
2025-12-09 20:56:29 +0000

    Usunięto wszystkie pliki PHP, tylko statyczne pliki

---
+++ pages/home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Strona domowa</title>
+</head>
+<body>
+    <h1>Witamy na stronie domowej!</h1>
+    <p>To jest przykładowa strona w folderze <code>pages</code>.</p>
+</body>
+</html>
